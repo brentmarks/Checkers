f6861222a25d4a5c24cc6bff8f95d11446e4b012
brentmarks <brentmaxwellmarks@gmail.com>
2016-11-13 21:12:13 -0500

    Add files via upload

--- Checkers/src/checkers/Checkers.java
+++ Checkers/src/checkers/Checkers.java
@@ -4,104 +4,50 @@ import java.util.Scanner;
 
 /**** @author brentmarks*/
 
-public class Checkers 
-{
-    
-    private static final Scanner sc = new Scanner(System.in);
+public class Checkers {
+   
     public static User current;
-    public static User Red;
-    public static User White;
+    public static User Red, White;
+    public static SimpleUI ui;
     
-    public static int[] initialLocationInput(int[] loc){
-        boolean cont = true, cont1 = false;
-        while(cont){
-            cont1 = false;
-            System.out.println(current.getName() + " please enter: original location of piece row col");
-            String input = sc.nextLine();
-            String[] slots = input.split(" ");
-            for(int i = 0; i < 2; i++){
-                try{
-                    loc[i] = Integer.parseInt(slots[i]);// -1 after to adjust for starting at zero
-                }catch(Exception e){
-                    cont1 = true;
-                    break;
-                }
-            }
-            cont = cont1;
-        }
-        return loc;
-    }
-    public static int[] moveInput(int[] loc){
-        boolean cont = true, cont1 = false;
-        while(cont){
-            cont1 = false;
-            System.out.println(current.getName() + " please enter: final location of piece you would like to move row col.");
-            String input1 = sc.nextLine();
-            String[] slots2 = input1.split(" ");
-            for(int i = 0; i < 2; i++){
-                try{
-                    loc[i + 2] = Integer.parseInt(slots2[i]);// -1 after to adjust for starting at zero
-                }catch(Exception e){
-                    cont1 = true;
-                    break;
-                }
-            }
-            cont = cont1;
-        }
-        return loc;
-    }
     public static void createUsers(){
-        System.out.println("Please enter your name for player red:");
-        String red = sc.nextLine();
-        Red = new User("r", red);
-        System.out.println("Please enter your name for player white:");
-        String white = sc.nextLine();
-        White = new User("w", white);
+        User[] users = ui.createUsers();
+        Red = users[0];
+        White = users[1];
     }
-    public static String skipOption(Board b, int x1, int y1){
-        if(b.skip(current.getColor(), x1, y1)){
-            System.out.println("Would you like to skip or move piece? (yes/no)");
-            String input = sc.nextLine();
-            return input;
-        }
-        return "";
-    }
-    
-    public static void main(String[] args) 
-    {
+    public static void Users(){
+        ui = new SimpleUI();
         Board b = Board.getInstance();
         b.printBoard();
         createUsers();
         boolean a = true, a1 = true;
         String c = "r";
         
-        while(a)
-        {
-            while(a1)
-            {
+        while(a){
+            while(a1){
                 current = c.equals("r") ? Red : White; 
                 int[] loc = new int[4];
                 
-                loc = initialLocationInput(loc);
-                String input3 = skipOption(b, loc[0], loc[1]);//System.out.println("Skip: " + b.skip(current.getColor(), loc[0], loc[1]));
+                loc = ui.initialLocationInput(b, current.getName(), loc, current.getColor());
+                String input3 = ui.skipOption(b, current.getColor(), loc[0], loc[1]);
 
-                loc = moveInput(loc);
+                loc = ui.moveInput(b, current.getName(), loc, current.getColor());
                 
                 if(input3.equals("yes")){
-                    a1 = !b.skipPiece(current.getColor(), loc[0], loc[1], loc[2], loc[3]);
+                    a1 = !b.skipPiece(current.getColor(), new Move(loc[0], loc[1]), new Move(loc[2], loc[3]));
                     boolean cont2 = !a1 && b.skip(current.getColor(), loc[2], loc[3]);
-                    while(cont2)
-                    {
+                    while(cont2){
                         //move starting values to account for new starting location
                         loc[0] = loc[2];
                         loc[1] = loc[3];
                         if(!a1 && b.skip(current.getColor(), loc[0], loc[1])){
-                            String input4 = skipOption(b, loc[0], loc[1]);
-                            loc = moveInput(loc);
-                            if(input4.equals("yes")){
-                                a1 = !b.skipPiece(current.getColor(), loc[0], loc[1], loc[2], loc[3]);
+                            String input4 = ui.skipOption(b, current.getColor(), loc[0], loc[1]);
+                            loc = ui.moveInput(b, current.getName(), loc, current.getColor());
+                            if(input4.equalsIgnoreCase("yes")){
+                                a1 = !b.skipPiece(current.getColor(), new Move(loc[0], loc[1]), new Move(loc[2], loc[3]));
                             }else{
-                                a1 = !b.movePiece(current.getColor(), loc[0], loc[1], loc[2], loc[3]);
+                                
+                                a1 = !b.movePiece(current.getColor(), new Move(loc[0], loc[1]), new Move(loc[2], loc[3]));
                                 cont2 = false;
                             }
                         }else{
@@ -109,14 +55,15 @@ public class Checkers
                         }
                     }
                 }else{
-                    a1 = !b.movePiece(current.getColor(), loc[0], loc[1], loc[2], loc[3]);
+                    a1 = !b.movePiece(current.getColor(), new Move(loc[0], loc[1]), new Move(loc[2], loc[3]));
                 }
-                System.out.println(loc[0] + " " + loc[1] + " " + loc[2] + " " + loc[3]);
-                
             }
             b.printBoard();
-            c = b.flipColor(c);//System.out.println(c);
+            c = b.flipColor(c);
             a1 = true;
         }
+    }
+    public static void main(String[] args) {
+        Users();
     }
 }
